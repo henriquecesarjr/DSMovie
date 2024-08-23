92bf2a488b3e127de3233a68bc63b5daf03b208d
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2024-08-23 10:37:45 -0300

    Swagger

--- src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dsmovie.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,13 +20,27 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/scores")
+@Tag(name = "Score", description = "Controller for Score movie")
 public class ScoreController {
 	
 	@Autowired
 	private ScoreService service;
-	
+
+	@Operation(
+			description = "Endpoint for save score",
+			summary = "Save score",
+			responses = {
+					@ApiResponse(description = "Ok", responseCode = "200"),
+					@ApiResponse(description = "Bad Request", responseCode = "400"),
+					@ApiResponse(description = "Unauthorized", responseCode = "401"),
+					@ApiResponse(description = "Forbidden", responseCode = "403"),
+					@ApiResponse(description = "Not Found", responseCode = "404"),
+					@ApiResponse(description = "Unprocessable Entity", responseCode = "422")
+			}
+	)
+	@SecurityRequirement(name = "bearerAuth")
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
-	@PutMapping
+	@PutMapping(produces = "application/json")
 	public ResponseEntity<MovieDTO> saveScore(@Valid @RequestBody ScoreDTO dto) {
 		MovieDTO movieDTO = service.saveScore(dto);
 		return ResponseEntity.ok().body(movieDTO);
